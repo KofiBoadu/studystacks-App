d76ae789ae8ba826feca81843c22932b4fb5bbf6
Daniel Boadu <mrboadu3@gmail.com>
2023-05-21 14:39:35 -0500

    learningLog Application

---
+++ learning_logs/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+# Register your models here.
+
+#registering Topic class or model with the admin site 
+from learning_logs.models import Topic,Entry_ofWhatUserLearnt
+admin.site.register(Topic)
+admin.site.register(Entry_ofWhatUserLearnt)
+
+
---
+++ learning_logs/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+#we will need to create models such as class to tell Django how to work 
+#with the data we will be providing .
+#Our users need to create a topic they are learning and we will need to keep 
+#time stamp of each learning logs .
+
+class Topic(models.Model):
+	"""A topic the user is learning about """
+	text= models.CharField(max_length=200)
+	date_added= models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		"""Return a string version of the model or our class"""
+		return self.text
+
+
+class Entry_ofWhatUserLearnt(models.Model):
+	"""what the user learned about a topic"""
+	topic= models.ForeignKey(Topic,on_delete=models.CASCADE)
+	text= models.TextField()
+	date_added= models.DateTimeField(auto_now_add=True)
+
+
+	class Meta:
+		verbose_name_plural="entries"
+
+	def __str__(self):
+		"""return a string """
+
+		if len(self.text) > 50:
+			return self.text[:50] + "...."
+		else:
+			return  self.text
